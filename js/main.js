95f604640f08823c6f50c349759b624292bde62f
whopid <raguzinadarya@yandex.ru>
2024-12-23 11:04:06 +0300

    всё

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// slider
+$(function () {
+  $('.sale__slider').slick({
+      prevArrow: '<button class="slick-btn slick-prev" style="position: absolute;top: 50%;transform: translateY(-50%);left: 0;z-index:1001"><img src="./images/slider/arrow-prew.svg" alt=""></button>',
+      nextArrow: '<button class="slick-btn slick-next" style="  position: absolute;top: 50%;transform: translateY(-50%);right: 0;z-index:1001"><img src="./images/slider/arrow-next.svg" alt=""></button>',
+  });
+});
